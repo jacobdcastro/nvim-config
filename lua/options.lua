87ed8927db638155e831124aa14c56eef1a614f4
Jacob D. Castro <jacob@jdc.dev>
2024-12-27 12:33:30 -0800

    start config over, with latest nvchad

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+require "nvchad.options"
+
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.autoindent = true
+o.smartindent = true
+o.showcmd = true
+o.expandtab = true
+o.scrolloff = 10
+o.inccommand = "split"
+o.smarttab  = true
+o.breakindent = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.splitbelow = true
+o.splitright = true
+o.splitkeep = "cursor"
